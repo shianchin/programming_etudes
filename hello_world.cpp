43e411b671895bb06a5fedb6f53413fdff164f0b
Shian Chin Cheang <shianchin3@gmail.com>
2016-10-10 21:05:36 +0800

    Hello world with different languages.

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,11 @@
-// my first program in C++
 #include <iostream>
 
 int main()
 {
-  std::cout << "Hello World!";
+  std::cout << "Hello!\n";
+  std::cout << "Bonjour!\n";
+  std::cout << "Guten!\n";
+  std::cout << "Ciao!\n";
+  std::cout << "Konnichiwa!\n";
+  std::cout << "Ni Hao!\n";
 }
